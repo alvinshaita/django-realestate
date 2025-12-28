a70a377271feab884b48d5057e507dc8470dda82
alvinshaita <alvinshaita@gmail.com>
2025-12-28 15:50:27 +0300

    start urls from current url path

--- realestate/settings.py
+++ realestate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w6rm%l&xim0ivll-li$u6fg8)6k8-$7uar^f#33ht5sutw8e!#'
+SECRET_KEY = 'ZBdiLiQ6OuWyamt6M6Lbd8QLXvJ4_SnyqTFu_TvT66ERLDqSrA'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -135,14 +135,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'realestate/static')
 ]
 
 # Media Folder Settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 
 # Messages
